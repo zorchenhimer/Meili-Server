dd52f03375c56c6b487d802d7cddf4349e9430f4
Nick Scheibenpflug <zorchenhimer@gmail.com>
2014-10-27 20:52:52 -0400

    added status to the http api test

--- http-test.py
+++ http-test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import time
 
 PORT_NUMBER = 8080
 
@@ -21,27 +22,40 @@ class TestHandler(BaseHTTPRequestHandler):
 	def do_GET(self):
 		if self.path == '/schedule.json' or self.path == '/schedule.xml':
 			self.send_response(200)
-			self.send_header('Content-type', 'text/plain')
+			if self.path[-4:] == '.xml':
+				self.send_header('Content-type', 'text/xml')
+			else:
+				self.send_header('Content-type', 'text/plain')
 			self.end_headers()
 
 			#self.wfile.write('Derp')
 			f = open('../doc' + self.path)
 			self.wfile.write(f.read())
 			f.close()
+
 		elif self.path.find('/status') == 0:
-			self.send_response(200)
-			self.send_header('Content-type', 'text/plain')
-			self.end_headers()
+			error = False
+			retmsg = ""
 
-			self.wfile.write("Server Status\n\n")
-			urivars = self.path[8:].split('&')
-			for pair in urivars:
-				self.wfile.write(pair + "\n")
-
-			self.wfile.write("\nUsing parse_urivars():\n")
-			parsed = parse_urivars(self.path[8:])
-			for key, val in parsed.iteritems():
-				self.wfile.write("\t" + key + ': ' + str(val) + "\n")
+			if self.path.find('?') == 7:
+				urivars = parse_urivars(self.path[8:])
+				if 'type' not in urivars:
+					error = True
+				else:
+					if urivars['type'] == 'servertime':
+						retmsg = str(int(time.time()))
+					else:
+						error = True
+			else:
+				retmsg = "General Stats\n\nTODO"
+			
+			if error == False:
+				self.send_response(200)
+				self.send_header('Content-type', 'text/plain')
+				self.end_headers()
+				self.wfile.write(retmsg)
+			else:
+				self.send_error(400, 'Invalid query: %s' % str(urivars))
 
 		else:
 			self.send_error(404, 'File Not Found: %s' % self.path)
