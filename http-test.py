6d85781805ec0378843f90b9125e5dfb3eac4729
Nick Scheibenpflug <zorchenhimer@gmail.com>
2014-10-30 03:48:25 -0400

    abstracted http request code a bit to reduce redundancy. do_GET() and do_POST() both point to process_request()

--- http-test.py
+++ http-test.py
@@ -20,7 +20,56 @@ def parse_urivars(varstring):
 	return retdict
 
 class TestHandler(BaseHTTPRequestHandler):
+	def process_request(self, basepath, urivars={}, method='get'):
+		print 'urivars: %s' % str(urivars) + "\n"
+		if basepath == '/schedule.json' or basepath == '/schedule.xml':
+			self.send_response(200)
+			if basepath[-4:] == '.xml':
+				self.send_header('Content-type', 'text/xml')
+			else:
+				self.send_header('Content-type', 'text/plain')
+			self.end_headers()
+
+			f = open('../doc' + basepath)
+			self.wfile.write(f.read())
+			f.close()
+		elif basepath == '/status':
+			error = True
+			retmsg = ''
+
+			if len(urivars) == 0:
+				error = False
+				retmsg = 'General stats\n\nTODO\n'
+			elif 'type' in urivars:
+				if 'servertime' in urivars['type']:
+					retmsg += str(int(time.time())) + "\n"
+					error = False
+				if 'lastupdate' in urivars['type']:
+					retmsg += str(int(time.time()) - 245) + "\n"
+					error = False
+				if 'displays' in urivars['type']:
+					retmsg += str(4) + "\n"
+					error = False
+
+			if error == True:
+				self.send_error(400, 'Invalid query: %s' % str(urivars))
+			else:
+				self.send_response(200)
+				self.send_header('Content-type', 'text/plain')
+				self.end_headers()
+				self.wfile.write(retmsg)
+		
+		else:
+			self.send_error(404, 'File Not Found: %s' % basepath)
+
+
 	def do_GET(self):
+		if self.path.find('?') != -1:
+			self.process_request(self.path[:self.path.find('?')], parse_urivars(self.path[self.path.find('?') + 1:]))
+		else:
+			self.process_request(self.path)
+
+		"""
 		if self.path == '/schedule.json' or self.path == '/schedule.xml':
 			self.send_response(200)
 			if self.path[-4:] == '.xml':
@@ -65,8 +114,11 @@ class TestHandler(BaseHTTPRequestHandler):
 		else:
 			self.send_error(404, 'File Not Found: %s' % self.path)
 		return
+		"""
 	
 	def do_POST(self):
+		self.process_request(self.path, parse_urivars(self.rfile.read( int(self.headers.getheader('content-length')))), 'post')
+		"""
 		if self.path == '/posted':
 			self.send_response(200)
 			self.send_header('Content-type', 'text/plain')
@@ -79,6 +131,7 @@ class TestHandler(BaseHTTPRequestHandler):
 			self.wfile.write("\n\n" + str(vars))
 		else:
 			self.send_error(404, 'File Not Found: %s' % self.path)
+		"""
 
 try:
 	server = HTTPServer( ('', PORT_NUMBER), TestHandler)
