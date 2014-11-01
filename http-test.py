b7f6ec08deec28c3cb118cca49d787fbb81b4ed4
Nick Scheibenpflug <zorchenhimer@gmail.com>
2014-10-31 23:12:15 -0400

    Moved all API logic to it's own class, removing it from the HTTP handler.

--- http-test.py
+++ http-test.py
@@ -3,11 +3,14 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import time
 import urllib
+import json
+import xml.etree.ElementTree as ET
 
 PORT_NUMBER = 8080
 
 def parse_urivars(varstring):
 	## varstring is the uri minus everything before, and including, the '?'.
+	print 'parsing varstring: %s' % str(varstring)
 	retdict = {}
 	urivars = varstring.split('&')
 	for pair in urivars:
@@ -19,48 +22,136 @@ def parse_urivars(varstring):
 	
 	return retdict
 
+## FFFFFFFFFUUUUUUUUUUu
+def dict_to_XML(rootname, struct):
+	print "Attempting to encode " + str(struct) + " into XML with root " + str(rootname)
+	root = ET.Element(rootname)
+	for key in struct:
+		var = struct[key]
+		if type(var) is type(dict()):
+			e = ET.SubElement(root, key)
+			_rabbit_hole_dict(e, key, var)
+		elif type(var) is type(list()):
+			for item in var:
+				e = ET.SubElement(root, key)
+				e.text = item
+	print "Finished attempt: " + str(ET.dump(root))
+	return ET.dump(root)
+
+def _rabbit_hole_dict(parent, name, struct):
+	for key, var in struct:
+		if type(var) is type(dict()):
+			e = ET.SubElement(parent, _rabbit_hole_dict(key, var))
+		elif type(var) is type(list()):
+			for item in var:
+				e = ET.SubElement(parent, key)
+				e.text = item
+				## TODO: rabbithole for lists
+
+class BaseAPIHandler():
+	def __init__(self):
+		self.__commands = {
+			'schedule': self.cmd_schedule,
+			'status':	self.cmd_status
+		}
+	
+	## Returns array: [content-type, content]
+	def do_command(self, command, vars):
+		## Check for `command` in self._commands
+		##   if exists, call function
+		if command.find('/') == 0:
+			command = command[1:]
+		if command in self.__commands:
+			return self.__commands[command](vars)
+		else:
+			raise Exception('Invalid command: %s' % command)
+	
+	def cmd_schedule(self, vars):
+		## Return the schedule according to `vars`
+		print 'schedule vars: %s' % str(vars)
+		path = ''
+		content_type = ''
+		content = ''
+		if 'format' in vars:
+			if vars['format'] == 'xml':
+				path = '../doc/schedule.xml'
+				content_type = 'text/xml'
+			elif vars['format'] == 'json':
+				content_type = 'application/json'
+				path = '../doc/schedule.json'
+		else:
+			content_type = 'application/json'
+			path = '../doc/schedule.json'
+
+		f = open(path, 'r')
+		content = f.read()
+		f.close()
+		
+		return [content_type, content]
+	
+	def cmd_status(self, vars):
+		## Return the status according to `vars`
+		content_type = ''
+		content_struct = {}
+		encoded_content = ''
+		
+		if 'type' in vars:
+			t = vars['type'].split(',')
+			if 'servertime' in t:
+				content_struct['servertime'] = int(time.time())
+			if 'lastupdate' in t:
+				content_struct['lastupdate'] = int(time.time() - 1343)
+			if 'displays' in t:
+				content_struct['displays'] = int(4)
+		else:
+			content_struct = {"status":"General Stats - TODO"}
+		
+		if 'format' in vars:
+			if vars['format'] == 'xml':
+				##content_type = 'error 501'
+				##encoded_content = dict_to_XML('status', content_struct)
+				raise Exception("501 Not Implemented")
+			elif vars['format'] == 'json':
+				content_type = 'application/json'
+				e = json.JSONEncoder()
+				encoded_content = e.encode(content_struct)
+		else:
+			content_type = 'application/json'
+			e = json.JSONEncoder()
+			encoded_content = e.encode(content_struct)
+		
+		return [content_type, encoded_content]
+
 class TestHandler(BaseHTTPRequestHandler):
 	def process_request(self, basepath, urivars={}, method='get'):
-		print 'urivars: %s' % str(urivars) + "\n"
-		if basepath == '/schedule.json' or basepath == '/schedule.xml':
-			self.send_response(200)
-			if basepath[-4:] == '.xml':
-				self.send_header('Content-type', 'text/xml')
-			else:
-				self.send_header('Content-type', 'text/plain')
-			self.end_headers()
-
-			f = open('../doc' + basepath)
-			self.wfile.write(f.read())
-			f.close()
-		elif basepath == '/status':
-			error = True
-			retmsg = ''
-
-			if len(urivars) == 0:
-				error = False
-				retmsg = 'General stats\n\nTODO\n'
-			elif 'type' in urivars:
-				if 'servertime' in urivars['type']:
-					retmsg += str(int(time.time())) + "\n"
-					error = False
-				if 'lastupdate' in urivars['type']:
-					retmsg += str(int(time.time()) - 245) + "\n"
-					error = False
-				if 'displays' in urivars['type']:
-					retmsg += str(4) + "\n"
-					error = False
-
-			if error == True:
-				self.send_error(400, 'Invalid query: %s' % str(urivars))
-			else:
-				self.send_response(200)
-				self.send_header('Content-type', 'text/plain')
-				self.end_headers()
-				self.wfile.write(retmsg)
+		api = BaseAPIHandler()
+			
+		if basepath == '/schedule.json':
+			basepath = '/schedule'
+			urivars['format'] = 'json'
+		elif basepath == '/schedule.xml':
+			basepath = '/schedule'
+			urivars['format'] = 'xml'
 		
-		else:
-			self.send_error(404, 'File Not Found: %s' % basepath)
+		print "= processing request =\nbasepath: %s\nurivars: %s" % (str(basepath), str(urivars))
+		
+		try:
+			response = api.do_command(basepath, urivars)
+			print '== Content-type: %s ==' % str(response[0])
+			print "== Content ==\n" + str(response[1]) + "\n== End Content =="
+			self.send_response(200)
+			self.send_header('Content-type', response[0])
+			self.end_headers()
+			self.wfile.write(response[1])
+		except Exception as e:
+			# hacky, i know. should really be a costom exception.
+			num = 400
+			msg = str(e)
+			if str(e).find(' '):
+				num = int(str(e)[:str(e).find(' ')])
+				msg = str(e)[str(e).find(' ') + 1:]
+				
+			self.send_error(num, msg)
 
 
 	def do_GET(self):
