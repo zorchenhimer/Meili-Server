cc5583cab3e51a37d0bc5ae9b550c21e92684564
Nick Scheibenpflug <zorchenhimer@gmail.com>
2015-02-28 20:39:47 -0500

    Added SQL script for dummy data.

---
+++ dbdummydata.sql
@@ -0,0 +1,14 @@
+-- A few east coast cities.
+INSERT INTO cities (city) VALUES
+    ("New York, NY"), ("Boston, MA"), ("Newark, NJ"), ("Albany, NY"), ("Hartford, CT"),
+    ("Philadelphia, PA"), ("Baltimore, MB"), ("Providence, RI");
+
+-- Some random bus companies I found.
+INSERT INTO companies (company) VALUES
+    ("Greyhound"), ("Wright Coaches"), ("Gary Express Inc"), ("Vision Tours"),
+    ("US Coach Tours"), ("E-Z Bus Inc"), ("First Priority");
+
+-- Dummy gates to work with.
+INSERT INTO gates (gate) VALUES
+    ("a1"), ("a2"), ("a3"), ("b1"), ("b2"), ("b3"), ("c1"), ("c2"), ("c3");
+
